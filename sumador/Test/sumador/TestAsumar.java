0a451c14cb6abdc3fb70e9d827457496cacf5dfa
Admin <Admin@Pc-W8>
2020-03-08 21:13:40 +0100

    Test para cadenas de una cifra

--- sumador/Test/sumador/TestAsumar.java
+++ sumador/Test/sumador/TestAsumar.java
@@ -7,8 +7,33 @@ import org.junit.Test;
 public class TestAsumar {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar();
+        
+        // Comprobar valor correcto rango
+        String resultado = sumi.mostrar("5");
+        assertEquals("5 = 5",resultado );
+        
+        // Comprobar valor correcto limite
+        resultado = sumi.mostrar("0");
+        assertEquals("0 = 0",resultado );
+        
+        // Comprobar valor correcto limite
+        resultado = sumi.mostrar("9");
+        assertEquals("9 = 9",resultado );
+        
+        // Comprobar valor no correcto
+        resultado=sumi.mostrar("x");
+        assertEquals("",resultado );
+        
+        // Comprobar valor no correcto
+        resultado=sumi.mostrar("21");
+        assertEquals("",resultado );
+        
+        // Comprobar valor no correcto 
+        resultado=sumi.mostrar("");
+        assertEquals("",resultado );
+        
+    }
 
 }
--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -2,4 +2,26 @@ package sumador;
 
 public class ASumar {
 
+	/**
+	 * Muestra el valor de la suma de todos y cada uno de los dígitos que componen un número.
+	 * @param cad : cadena compuesta por digitos de (0-9).  
+	 * @return  : los digitos de cad separados por el simbolo + y el valor de la suma de todos sus digitos, en caso de error devuelve cadena vacia.
+	 */
+	public String mostrar(String cad) {
+        String numero = cad;
+        char digito;
+                
+        // Compruebo que la longitud es correcta =1
+        if (cad.length()==1) {
+        	digito=cad.charAt(0);
+        	
+        	// Compruebo que es un digito numerico valido
+        	if ( digito>='0' && digito<='9') {
+        		// Respuesta 
+                return (numero + " = " + numero);
+            } 
+        }
+        // Respuesta por fallo de formato
+        return "";
+	}
 }
